da8fbda088582d6c4c88257755c404c0a177bf57
Gintautas Viliūnas <g.viliunas@student.tudelft.nl>
2025-05-07 14:27:43 +0200

    link budget slight update

--- Link budget/linkBuddget.py
+++ Link budget/linkBuddget.py
@@ -1,10 +1,14 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
+c = 3e8
+k = 1.38e-23  # Boltzmann constant in J/K
+T = 290  # Noise temperature in Kelvin
+'''
 def calculate_link_budget(frequency_mhz, distance_km, tx_power_dbm, tx_gain_db, rx_gain_db, bandwidth_mhz, noise_figure_db):
     # Constants
-    c = 3e8  # Speed of light in m/s
-    k = 1.38e-23  # Boltzmann constant in J/K
-    T = 290  # Noise temperature in Kelvin
+      # Speed of light in m/s
+    
 
     # Convert frequency to Hz and distance to meters
     frequency_hz = frequency_mhz * 1e6
@@ -47,7 +51,7 @@ print("Link Budget Results:")
 for key, value in link_budget.items():
     print(f"{key}: {value:.2f} dB")
 
-
+'''
 #minimize power for given distance
 
 
@@ -58,12 +62,33 @@ for key, value in link_budget.items():
 
 
 #graph distancce vs power for  given snr and bandwith
-def power_vs_distance(frequency_mhz, tx_power_dbm, tx_gain_db, rx_gain_db, bandwidth_mhz, noise_figure_db):
-    distance
-    powers = []
+def power_vs_distance(distance,frequency, diameter_antenna,snr_db,bandwidth=20e6, noise_figure_db=5):
+    noise_figure = 10**(noise_figure_db/10) # Convert dB to linear scale
+    l_fs_db = 10 * np.log10((4 * np.pi * distance * frequency / c)**2) # Free space loss in dB
+    l_misc = 2.6 # Miscellaneous losses (e.g., connectors, cables, atmosphere etc.) rnadom web shit
+    rx_gain_db = 0 # Receiver gain in dB (assumed to be 0 for simplicity)
+   
+    tx_gain_db = 10 * np.log10(4*np.pi*0.75*np.pi*(diameter_antenna/2)**2 *(frequency/c)**2)
+   
+    P_rx_without_tx=tx_gain_db+rx_gain_db - l_fs_db-l_misc
+    noise = 10* np.log10(k*(noise_figure-1)*T*bandwidth) # Noise power in dBm
+    Ptx=noise + snr_db - P_rx_without_tx
 
-    for distance_km in distances:
-        link_budget = calculate_link_budget(frequency_mhz, distance_km, tx_power_dbm, tx_gain_db, rx_gain_db, bandwidth_mhz, noise_figure_db)
-        powers.append(link_budget["Received Power (dBm)"])
+    #convert dbm to linear scale
+    Ptx = 10**((Ptx)/10) # Convert dBm to linear scale
+   
+    
 
-    return powers
+    return Ptx
+#plot power vs distance
+distances = np.linspace(1, 100, 100)  # Distance from 1 km to 100 km
+acceptable_snr_db = 10  # Acceptable SNR in dB
+antenna_diameter = 3  # Antenna diameter in meters
+distances*= 1000
+powers= [power_vs_distance(distance, 1.6e9, antenna_diameter, acceptable_snr_db, bandwidth=20e6, noise_figure_db=5) for distance in distances]
+plt.plot(distances/1000, powers)
+plt.xlabel('Distance (km)')
+plt.ylabel('Transmitter Power (Watts)')
+plt.title('Transmitter Power vs Distance')
+plt.grid()
+plt.show()
