77429c385650e2272a9747dcd6dccf023e39678d
Midas1234567 <132276487+Midas1234567@users.noreply.github.com>
2025-05-07 11:56:35 +0200

    Update wingbox.py

--- wingbox.py
+++ wingbox.py
@@ -1,8 +1,4 @@
-class wingbox:
-    def __init__(self, material_properties, airfoil_thickness, loads, safety_factor):
-        self.material_properties = material_properties
-        self.airfoil_thickness = airfoil_thickness
-        self.loads = loads
-        self.safety_factor = safety_factor
-        self.type = type
-   
+import aerosandbox as asb
+
+airfoil = asb.Airfoil("sd7037")
+print(airfoil.coordinates)
