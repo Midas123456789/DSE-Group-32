27107ea01ec77149c38b616fe13d798fd292bebf
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2025-05-07 12:14:56 +0200

    First estimate for vehicle trajectory

---
+++ Vehicle.py
@@ -0,0 +1,21 @@
+class Vehicle:
+
+    def __init__(self, latvelocity, linvelocity, radius, startpostions):
+
+        self.latvelocity = latvelocity
+        self.linvelocity = linvelocity
+
+        self.radius = radius
+        self.startpostions = startpostions
+
+    def trajectory(self):
+        """
+        Determine the trajectory of the vehicle. with starting position and velocity
+        """
+        return
+
+    def endurance(self):
+        """
+        Determine the time that the HAPS can be operated
+        """
+        return
