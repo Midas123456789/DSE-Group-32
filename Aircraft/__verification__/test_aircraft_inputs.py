93cc8937d0bb11ded714cc952ca500b2e610d734
mikeinthout <mikeinthout@tudelft.nl>
2025-05-16 13:53:49 +0200

    rename main

--- Aircraft/__verification__/test_aircraft_inputs.py
+++ Aircraft/__verification__/test_aircraft_inputs.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from main import AircraftInputs
+from Aircraft.weight_performance_main import AircraftInputs
 
 class TestAircraftInputs(unittest.TestCase):
 
